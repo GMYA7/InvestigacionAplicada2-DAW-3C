04292072b53775873961a0c26cf2b624b806ba27
Diego-Melara <109252124+Diego-Melara@users.noreply.github.com>
2022-10-09 22:03:41 -0600

    Add files via upload

---
+++ js/localStorage.js
@@ -0,0 +1,19 @@
+function crearUsuario(){
+    var nombre = document.getElementById("nom").value;
+    var apellido = document.getElementById("ape").value;
+    var correo = document.getElementById("correo").value;
+    var telefono = document.getElementById("tel").value;
+
+    var usuario = {
+        nombre : nombre,
+        apellido : apellido,
+        correo: correo,
+        telefono: telefono
+    }; 
+    var cadenaUsuario = JSON.stringify(usuario);
+    localStorage.setItem(nombre, cadenaUsuario);
+    var nome =  localStorage.getItem(nombre);
+    console.log(nome);
+    console.log("Usuario agregado");
+    alert(cadenaUsuario);
+}
